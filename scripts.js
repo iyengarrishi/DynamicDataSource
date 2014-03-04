9c0bf94218dbdd198ad9dcb083afb13801f1e897
iyengarrishi <ri2195@columbia.edu>
2014-03-04 12:47:24 -0500

    added a second chart after selecting a section of the data

--- scripts.js
+++ scripts.js
@@ -25,7 +25,34 @@ function dataLoaded(UNEMPDATA) {
 	var myChart = new google.visualization.LineChart(document.getElementById("myChartDiv"));
 
 	var chartOptions = {
-          title: "Unemployment Trends"
+          title: "Unemployment Trends", 
+        };
+
+	myChart.draw(gDataTable, chartOptions);
+	
+
+}
+
+function dataLoaded2(UNEMPDATA) {
+
+	console.log(UNEMPDATA);
+	
+	var gDataTable = new google.visualization.DataTable();
+	
+	//When I add columns, the first parameter is the datatype in that column
+	//The second parameter is the name of the column
+	
+	gDataTable.addColumn('string', UNEMPDATA.columns[0]);
+	gDataTable.addColumn('number', UNEMPDATA.columns[1]);
+	
+	gDataTable.addRows(UNEMPDATA.rows)
+
+
+	
+	var myChart = new google.visualization.LineChart(document.getElementById("myChartDiv2"));
+
+	var chartOptions = {
+          title: "Unemployment Trends", 
         };
 
 	myChart.draw(gDataTable, chartOptions);
@@ -40,7 +67,10 @@ function googleLoaded() {
 	//Inside of the get, we see the filename, the function name and the file type
 	//Instead of loading data from a static JSON file, I will load it from a Google Fusion Table
 	
-	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1sqxKGNmK_aKGBk1hGpO8xyfGXxRnOoWUCCux3OsX&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", dataLoaded, "json");
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1sqxKGNmK_aKGBk1hGpO8xyfGXxRnOoWUCCux3OsX+WHERE+DATE<'1979-12-01'&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", dataLoaded2, "json");
+
+	
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1sqxKGNmK_aKGBk1hGpO8xyfGXxRnOoWUCCux3OsX+WHERE+DATE>'1979-12-01'&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", dataLoaded, "json");
 
    
 
